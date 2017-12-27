6c69359421d503a30969304d11cb403ddccf9874
Tomas <tomasholderness@gmail.com>
2017-12-26 20:05:22 -0500

    pep8 style

--- floodlight/_sequence.py
+++ floodlight/_sequence.py
@@ -1,5 +1,7 @@
 """Construct sequences of light patterns"""
 from _pattern import Pattern
+
+
 class Sequence():
     """Build light event sequences using Pattern class"""
     def __init__(self, config):
@@ -9,7 +11,8 @@ class Sequence():
 
     def online(self):
         """Returns online sequence"""
-        sequence = [{'pattern': self.pattern.online(), 'timing': 0.1}] #heartbeat
+        # heartbeat
+        sequence = [{'pattern': self.pattern.online(), 'timing': 0.1}]
         sequence.append({'pattern': self.pattern.online(), 'timing': 0})
         return (sequence)
 
@@ -17,27 +20,28 @@ class Sequence():
         return ([{'pattern': self.pattern.error(), 'timing': 0}])
 
     def flood(self, count):
-        sequence = [{'pattern': self.pattern.online(), 'timing': 0.1}] #heartbeat
+        # heartbeat
+        sequence = [{'pattern': self.pattern.online(), 'timing': 0.1}]
         sequence.append({'pattern': self.pattern.flood(count), 'timing': 0})
         return (sequence)
 
     def rainfall(self, count):
         """Raindrop animation method"""
-        # get raindrop from pattern
+        # Get raindrop from pattern
         sequence = []
         sequence.append({'pattern': self.pattern.new_report, 'timing': 2})
         sequence.append({'pattern': self.pattern.flood(count), 'timing': 0})
         return sequence
 
     def build(self, last_count, new_count):
-        """Accepts previous and new report count values and returns light pattern sequence"""
+        """Accepts previous and new report counts, returns light sequence"""
 
         # Logic to set sequence based on latest report count
         if new_count == 0:
             return (self.online())
         elif new_count > last_count:
             return (self.rainfall(new_count))
-        elif new_count > 0 :
+        elif new_count > 0:
             return (self.flood(new_count))
         else:
             return (self.error())
