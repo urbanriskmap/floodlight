9a4576dc04904c0569afe47823ab8f06246b0246
Tomas <tomasholderness@gmail.com>
2017-12-27 16:55:45 -0500

    working requests mock

--- tests/test_floodlight.py
+++ tests/test_floodlight.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import patch
+from unittest.mock import MagicMock
 import sys
 import os
 import json
@@ -16,13 +17,31 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../f
 import floodlight
 
 config = json.load(open('config.json'))
+# Use short connection for tests
+config['fadecandy']['long_connection'] = False
 
 class TestFloodlight(unittest.TestCase):
+
+    # This method will be used by the mock to replace requests.get
+    def mocked_requests_get(*args, **kwargs):
+        class MockResponse:
+            def __init__(self, json_data, status_code):
+                print('yippeeeeeeeee')
+                self.json_data = json_data
+                self.status_code = status_code
+
+            def json(self):
+                yield self.json_data
+
+        if args[0] == 'http://someurl.com/test.json':
+            yield MockResponse({"key1": "value1"}, 200)
+        elif args[0] == 'http://someotherurl.com/anothertest.json':
+            yield MockResponse({"key2": "value2"}, 200)
+        yield MockResponse(None, 404)
 
     def test_init_error(self):
         """Catches error with Fadecandy connection"""
         try:
-            config['fadecandy']['long_connection'] = False
             floodlight.FloodLight(config)
         except Exception as e:
             self.assertEqual('Could not connect to Fadecandy server at %s' % config['fadecandy']['server'], str(e))
@@ -32,18 +51,31 @@ class TestFloodlight(unittest.TestCase):
         """Can initialize instance of FloodLight class"""
         # disconnect
         mock_can_connect.return_value = True
-        config['fadecandy']['long_connection'] = False
         self.assertIsInstance(floodlight.FloodLight(config), floodlight.FloodLight)
 
     @patch('_opc.Client.can_connect')
     def test_get_report_count_error(self, mock_can_connect):
         mock_can_connect.return_value = True
-        config['fadecandy']['long_connection'] = False
         config['cognicity']['reports_endpoint'] = 'http://localhost'
         count = floodlight.FloodLight(config)._get_report_count()
         self.assertEqual(count, -1)
 
-        
+
+    @patch('_opc.Client.can_connect')
+    def test_get_report_count(self, mock_can_connect):
+        with patch('requests.get') as mock_request:
+            mock_request.return_value = MagicMock(status_code=200, json=lambda : json.loads(json.dumps({'result':{'features':[0,1,2]}})))
+            config['cognicity']['reports_endpoint'] = 'http://localhost'
+            count = floodlight.FloodLight(config)._get_report_count()
+            self.assertEqual(count, 3)
+
+        #mock_can_connect.return_value = True
+        #mocked_requests_get.return_value = MagicMock(status_code=200, response=json.dumps({'spam':{'features':[0,1]}}))
+
+
+
+
+
     """def test_client(self, mock_api_call):
         mock_api_call.return_value = MagicMock(status_code=200, response=json.dumps({'key':'value'}))
         fl._send_sequence([{pattern:[(1,2,3)],timing:0}])"""
